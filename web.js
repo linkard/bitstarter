305e889537fe9123839bd3830fe79be214456611
Linkard Knight <iLinkard@gmail.com>
2013-10-07 07:00:53 +0000

    Sleeep

--- web.js
+++ web.js
@@ -10,7 +10,7 @@ var buf = new Buffer(256);
 
 app.use(express.logger());
 app.get('/', function(request, response) {
-    buf=fs.readFileSync('bitstarter/index.html');
+    buf=fs.readFileSync('index.html');
     response.send(buf.toString('utf-8'));
 });
 
