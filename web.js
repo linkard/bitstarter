a67df3e2535079c0b4bc6ebba3c06b37e481983d
Linkard Knight <iLinkard@gmail.com>
2013-10-07 05:20:50 +0000

    Prueba de comit1

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var app = express();
-app.use(express.logger());
+var fs = require('fs');
+var buffer = new Buffer(fs.readFileSync("/home/ubuntu/hw3/bitstarter/index.html"));
+console.log(process.argv.length);
 
+
+console.log(buffer.toString('utf-8'));
+
+app.use(express.logger());
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send("Esto es lo que debe mostrar::: "+buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
