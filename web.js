16e627633eceda2e42be8d2ea970b46795c3d053
Linkard Knight <iLinkard@gmail.com>
2013-10-07 06:04:48 +0000

    Ahora si el definitivo

--- web.js
+++ web.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
-var buffer = new Buffer(fs.readFileSync("/home/ubuntu/hw3/bitstarter/index.html"));
-console.log(process.argv.length);
+var buf = new Buffer(256);
+
+//veamos
+buf=fs.readFileSync('/home/ubuntu/hw3/bitstarter/index.html'); 
 
 
-console.log(buffer.toString('utf-8'));
+console.log(buf.toString('utf-8'));
 
 app.use(express.logger());
 app.get('/', function(request, response) {
-  response.send("Esto es lo que debe mostrar::: "+buffer.toString('utf-8'));
+  response.send(buf.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
